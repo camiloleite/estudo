c40ad964749825f27e3bfc3e63776f587a368130
camiloleite <camilo.leite22@gmail.com>
2019-07-15 23:53:25 -0300

    html e ts ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormGroup, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  contato : FormGroup = new FormGroup({
+    nome : new FormControl(null),
+    telefone : new FormControl(null),
+    email: new FormControl(null)
+  })
   constructor() { }
 
   ngOnInit() {
+  }
+
+  salvarContato(){
+    console.log(this.contato.value)
+    this.contato.reset()
+      
   }
 
 }
